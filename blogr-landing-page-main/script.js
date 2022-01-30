73b43a2e555752e20cb23b2509c9027e3a455be5
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-01-31 02:42:06 +0530

    landing page completed

--- blogr-landing-page-main/script.js
+++ blogr-landing-page-main/script.js
@@ -1,7 +1,26 @@
-var btn,dropcontent,arrow;
+var btn,dropcontent,arrow,mobbtn,mob,menubtn;
 btn = document.getElementsByClassName('btn')
+mobbtn = document.getElementsByClassName('mob-btn')
 dropcontent = document.getElementsByClassName('dropdown-content');
+mobdrop = document.getElementsByClassName('mob');
 arrow = document.getElementsByClassName('arrow');
+menubtn = document.getElementById('menu-img')
+mobilemenu = document.getElementsByClassName('mobile-menu')[0];
+
+menubtn.addEventListener('click', () => {
+    if (mobilemenu.classList.contains('mob-menu-show')){
+        menubtn.src = "images/icon-hamburger.svg";
+    }
+    else {
+        menubtn.src = "images/icon-close.svg";
+    }
+    mobilemenu.classList.toggle('mob-menu-show')
+    for ( let k = 0; k < mobdrop.length;k++){
+        mobdrop[k].classList.remove('mob-show')
+        arrow[k].classList.remove('up');
+    }
+})
+
 
 for (let i = 0; i < btn.length; i++){
     btn[i].addEventListener('click', () =>{
@@ -16,3 +35,29 @@ for (let i = 0; i < btn.length; i++){
         arrow[i].classList.toggle('up')
     })
 }
+
+
+for (let i = 0; i < mobbtn.length; i++){
+    mobbtn[i].addEventListener('click', () =>{
+        for (let j = 0; j < mobdrop.length; j++) {
+            if ( i != j){
+                mobdrop[j].classList.remove('mob-show');
+                arrow[j].classList.remove('up')
+            }
+        }
+        var dropdown = mobbtn[i].nextElementSibling;
+        dropdown.classList.toggle('mob-show')
+        arrow[i].classList.toggle('up')
+    })
+}
+
+window.addEventListener('click', (e) => {
+    if (e.target != mobilemenu  && e.target.parentNode != mobilemenu
+        && e.target.parentNode.parentNode != mobilemenu && 
+        e.target.parentNode.parentNode.parentNode != mobilemenu
+        && e.target != menubtn){
+        mobilemenu.classList.remove('mob-menu-show')
+        menubtn.src = "images/icon-hamburger.svg";
+    }
+})
+
