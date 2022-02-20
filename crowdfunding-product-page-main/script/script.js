2439873198508312de77140dd182bad6988a8a03
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-02-20 16:17:53 +0530

    added icon image background transition

--- crowdfunding-product-page-main/script/script.js
+++ crowdfunding-product-page-main/script/script.js
@@ -1,5 +1,7 @@
 var menu_btn = document.getElementsByClassName('btn-box')[0];
 var mob_menu = document.getElementsByClassName('mob-menu')[0];
+var val = document.getElementById('val');
+console.log(val.innerText);
 menu_btn.addEventListener('click', ()=> {
     var img = document.getElementById("btn-img")
     if (mob_menu.classList.contains('menu-show')){
@@ -10,3 +12,27 @@ menu_btn.addEventListener('click', ()=> {
     }
     mob_menu.classList.toggle("menu-show")
 })
+
+
+function exec() {
+    setInterval( function addval(){
+        value = parseInt(val.innerText);
+        if (value != 56){
+            value = value + 1;
+            val.innerText = value;
+        }
+        else {
+            val.innerText = value;
+        }
+    },100)
+}
+function bookmark() {
+    var _bookmark = document.getElementById('bookmark');
+    var head = document.getElementsByTagName("HEAD")[0];
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.href = "style/spl_style.css";
+    head.append(link);
+    _bookmark.style.color = "hsl(176, 72%, 28%)";
+    _bookmark.style.backgroundColor = "white";
+}
