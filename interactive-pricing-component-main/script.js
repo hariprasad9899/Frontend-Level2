2fa35c48f76fea20e773b20769caef1f96f83db9
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-03-02 03:20:36 +0530

    all done

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -1,13 +1,30 @@
-var slider,val;
+var slider,val,toggle;
 
 slider = document.getElementsByClassName('slider')[0];
+toggle_btn = document.getElementsByClassName('toggle')[0];
+inner_round = document.getElementsByClassName('inner')[0];
 
 slider.oninput = function() {
     cyan_calc = this.value;
-    gray_calc = 100 - this.value;
+    gray_calc = 0 - this.value;
 
-    console.log(cyan_calc,gray_calc)
-
+    // this will change the slider track 
     slider.style.setProperty('--webkitProgressCyanPercent',`${cyan_calc}%`);
-    slider.style.setProperty('--webkitProgressGrayPercent',`${gray_calc}%`);
+    slider.style.setProperty('--webkitProgressGrayPercent',`${gray_calc}%`);  
+
+    // this will change the color of the thumb when movin
+    if (this.value%2 == 0) {
+        rgb_value = 240 - this.value;
+        var thumb_color_value = getComputedStyle(slider);
+        thumb_color = thumb_color_value.getPropertyValue('--WebkitThumbColorValue'); 
+        slider.style.setProperty('--WebkitThumbColorValue',`${rgb_value}`);
+        console.log(this.value,rgb_value)
+    }   
+}
+
+
+// transition for toggle button
+toggle_btn.onclick = function() {
+    toggle_btn.classList.toggle('bright');
+    inner_round.classList.toggle('round-move');
 }
