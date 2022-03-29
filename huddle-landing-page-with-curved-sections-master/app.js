0f4f194756a0ec9c81804e336b851f2423e2f034
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-03-29 23:50:43 +0530

    all done

---
+++ huddle-landing-page-with-curved-sections-master/app.js
@@ -0,0 +1,39 @@
+let input = document.getElementById('input');
+let btn = document.getElementById('btn-sub');
+
+btn.onclick = function() {
+    let txt = input.value;
+    let result = ValidateEmail(txt);
+    if (result) {
+        error.style.opacity = "0";
+        input.style.outline = "none";
+    } else {
+        error.style.opacity = "1";
+        input.style.outline = "1px solid red";
+    }
+}
+
+function ValidateEmail(val) {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (val.match(validRegex)) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+input.oninput = function() {
+    error.style.opacity = "0";
+    input.style.outline = "none";
+}
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      btn.click();
+    }
+});
+
+
+
+
+
