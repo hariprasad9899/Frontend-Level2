18e5ac5970709d4a8c634e4b986ce49d80c984d3
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-03-30 02:28:36 +0530

    all done

--- advice-generator-app-main/app.js
+++ advice-generator-app-main/app.js
@@ -1,15 +1,17 @@
-let url = "	https://api.adviceslip.com/advice";
+let url = "https://api.adviceslip.com/advice";
 let btn = document.getElementById('btn');
 let txt = document.getElementById('advice')
+let ad_id = document.getElementById('ad-id');
 
 async function getAdvice() {
     const response = await fetch(url);
     const data = await response.json();
-    return data.slip.advice;
+    return data;
 }
 
 
 btn.onclick = async function() {
     let val = await getAdvice();
-    txt.innerText = val;
+    txt.innerText = val.slip.advice;
+    ad_id.innerText = val.slip.id;
 }
